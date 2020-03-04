ca78f63894da5ba80764c4dc557dad7ccfa63f9d
yuchaowni <jiang@wni.com>
2020-03-04 17:36:23 +0900

    add argparse

--- local_data_to_motion_vector.py
+++ local_data_to_motion_vector.py
@@ -1,6 +1,8 @@
 # written by Yuchao Jiang on 2020.2.27
 # opencv 3.4.9
 # python 3.7.6
+# generate u,v binary files: python **.py
+# generate u,v binary files + png: python **.py --output_figure
 
 import os
 import cv2
@@ -14,7 +16,7 @@ from mpl_toolkits.basemap import Basemap
 import argparse
 
 
-def main(output_figure =True):
+def main(output_figure = False):
     t0 = time()
     data_folder = "/Users/jiang/data/kakuho_dev"  ## in local computer 
     output_folder = "./output_vector"
@@ -23,9 +25,11 @@ def main(output_figure =True):
     bin_files = glob.glob(os.path.join(data_folder,"*ints.1km.bin"))  # 288 = 12*24
     bin_files.sort()
     now_files = bin_files[-2:]
+    print("--------------- using these two files ---------------")
     print(*now_files, sep = "\n")
     datetime_str = now_files[-1].split("/")[-1].split(".")[0]
     datetime_object = datetime.strptime(datetime_str,"%Y%m%d%H%M")
+    print("--------------- now time is: ---------------")
     print(datetime_object)
 
     time_step = 5 * 60 # seconds
@@ -44,34 +48,36 @@ def main(output_figure =True):
 
     u = ma.array(data = delta[:,:,0], mask = mask,fill_value = 0)
     v = ma.array(data = delta[:,:,1], mask = mask,fill_value = 0)
-
-    # plot motion vector on base map
-    plt.figure(dpi=150)
-    m = Basemap(llcrnrlat=20,urcrnrlat=48, llcrnrlon=118, urcrnrlon=150,resolution = "i")
-    lon = np.linspace(118.00625, 149.99375, 2560)
-    lat = np.linspace(20.004167, 47.995833, 3360)
-    lons, lats = np.meshgrid(lon, lat)
-    m.contourf(lons, lats, data_scaled[-1], levels=list(range(1,255)),cmap='jet' )
-    m.drawcoastlines(color='black')
-    m.drawcountries()
-    m.drawmeridians(np.arange(118, 150, 5), labels=[1,0,0,1])# left, right, top or bottom
-    m.drawparallels(np.arange(20, 48, 5), labels=[1,0,1,0])
-    plt.colorbar()
-    x,y = m(lon, lat)
-    skip = 60
-    plt.quiver(x[::skip], y[::skip], u[::skip,::skip], v[::skip,::skip], scale = 100,  headwidth = 3, alpha=0.4)
-    m.pcolormesh(lons, lats, mask, cmap= "binary", alpha=0.002)
-    plt.title(f"{datetime_object.strftime('%Y_%m_%d %H:%M ')}basemap_motion_vector")
-    plt.savefig(f"{output_folder}/{datetime_str}_basemap_motion_vector.png",format = "png",bbox_inches='tight')
-
     # save data to binary 
     u.data.tofile(f"{output_folder}/{datetime_str}_u.bin")
     v.data.tofile(f"{output_folder}/{datetime_str}_v.bin")
+
+
+    if output_figure == True:
+        # plot motion vector on base map
+        plt.figure(dpi=150)
+        m = Basemap(llcrnrlat=20,urcrnrlat=48, llcrnrlon=118, urcrnrlon=150,resolution = "i")
+        lon = np.linspace(118.00625, 149.99375, 2560)
+        lat = np.linspace(20.004167, 47.995833, 3360)
+        lons, lats = np.meshgrid(lon, lat)
+        m.contourf(lons, lats, data_scaled[-1], levels=list(range(1,255)),cmap='jet' )
+        m.drawcoastlines(color='black')
+        m.drawcountries()
+        m.drawmeridians(np.arange(118, 150, 5), labels=[1,0,0,1])# left, right, top or bottom
+        m.drawparallels(np.arange(20, 48, 5), labels=[1,0,1,0])
+        plt.colorbar()
+        x,y = m(lon, lat)
+        skip = 60
+        plt.quiver(x[::skip], y[::skip], u[::skip,::skip], v[::skip,::skip], scale = 100,  headwidth = 3, alpha=0.4)
+        m.pcolormesh(lons, lats, mask, cmap= "binary", alpha=0.002)
+        plt.title(f"{datetime_object.strftime('%Y_%m_%d %H:%M ')}basemap_motion_vector")
+        plt.savefig(f"{output_folder}/{datetime_str}_basemap_motion_vector.png",format = "png",bbox_inches='tight')
+
     print(f"time cost: {time()-t0:.2f} seconds")
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser( description= "input 2 radar images and output u,v vectors ")
-    parser.add_argument('--output_figure', type=bool, default= True,
-                        help='whether save OF vector figure, could be time-consuming')
+    parser = argparse.ArgumentParser(description= "input 2 radar images and output u,v vectors ")
+    parser.add_argument('--output_figure', action = "store_true",
+                        help='If output (basemap + vector) figure, will takes 30 seconds')
     args = parser.parse_args()
-    print(args)
+    main(args.output_figure)
